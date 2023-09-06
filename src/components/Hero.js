131d26c078f7de2cc31f515fd5fd2a80b370c118
ruthfeezota <110999344+ruthfeezota@users.noreply.github.com>
2023-09-06 13:02:36 -0500

    update 2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -118,7 +118,7 @@ const Brand = () => (
                                 className="w-full px-3 py-2.5 text-gray-200 bg-gray-700 focus:bg-gray-900 duration-150 outline-none rounded-lg shadow sm:max-w-sm sm:w-auto"
                             />
                             <button className="flex items-center justify-center gap-x-2 py-2.5 px-4 mt-3 w-full text-sm text-black font-medium bg-red-300 hover:bg-sky-400 active:bg-sky-600 duration-150 rounded-lg sm:mt-0 sm:w-auto">
-                                Contact Me
+                                Contact
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
                                 </svg>
@@ -136,7 +136,7 @@ const Brand = () => (
                         </div>
                     </div>
                 </div>
-                <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg" style={{ background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)" }}></div>
+               
             </section>
         </div>
   )
