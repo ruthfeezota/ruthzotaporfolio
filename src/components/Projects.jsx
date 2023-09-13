97777e15986bde8637742e96c6fe22e40facb406
ruthfeezota <110999344+ruthfeezota@users.noreply.github.com>
2023-09-13 18:57:26 -0500

    update spacing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
         <section className='py-4 text-white bg-gray-900' id='Project'>
         <div className="max-w-screen-xl mx-auto px-4 md:px-8">
             <div className='relative max-w-xl mx-auto sm:text-center'>
-                <h3 className=' text-3xl font-semibold sm:text-4xl text-red-300'>
+                <h3 className='text-3xl font-semibold sm:text-4xl text-red-300'>
                     Portfolio
                 </h3>
                 <p className="mt-3 text-white">
@@ -26,21 +26,20 @@ function Projects() {
 
         </div>
 
-
-    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
   <div class="-m-1 flex flex-wrap md:-m-2">
     <div class="flex w-1/2 flex-wrap">
 
-      <div class="w-1/2 p-1 md:p-2 group-hover:opacity-50">
-      <a href="https://mionails.ru/" target="_blank" rel="noreferrer">
+      <div class="w-1/2 p-1 md:p-2 group-hover:opacity-50 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
+      <a href="https://takespruce.com/" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
           class="block h-full w-full rounded-lg object-cover object-center"
-          src={NailPhoto} />
+          src={CBDoil} />
     </a>
       </div>
 
-      <div class="w-1/2 p-1 md:p-2">
+      <div class="w-1/2 p-1 md:p-2 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
       <a href="https://freckbeauty.com/" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
@@ -48,7 +47,7 @@ function Projects() {
           src={Roseoil} />
           </a>
       </div>
-      <div class="w-full p-1 md:p-2">
+      <div class="w-full p-1 md:p-2 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
       <a href="https://coffeeopia.com/" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
@@ -57,8 +56,8 @@ function Projects() {
           </a>
       </div>
     </div>
-    <div class="flex w-1/2 flex-wrap">
-      <div class="w-full p-1 md:p-2">
+    <div class="flex w-1/2 flex-wrap ">
+      <div class="w-full p-1 md:p-2 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
       <a href="https://trilogeneseeds.com/" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
@@ -66,15 +65,17 @@ function Projects() {
           src={Hempoil} />
           </a>
       </div>
-      <div class="w-1/2 p-1 md:p-2">
-      <a href="https://takespruce.com/" target="_blank" rel="noreferrer">
+      <div class="w-1/2 p-1 md:p-2 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
+      <a href="https://mionails.ru/" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
           class="block h-full w-full rounded-lg object-cover object-center"
-          src={CBDoil} />
+          src={NailPhoto}  />
           </a>
       </div>
-      <div class="w-1/2 p-1 md:p-2">
+
+
+      <div class="w-1/2 p-1 md:p-2 outline-none shadow-md focus:shadow-none focus:ring-2 ring-offset-2  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
       <a href="https://www.leafglobalfintech.com" target="_blank" rel="noreferrer">
         <img
           alt="gallery"
