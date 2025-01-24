ee33176ff311207535c29fca91ae03214e48d4b1
“Ruth <ruthzota@gmail.com>
2025-01-23 18:27:41 -0600

    Added Google Analitics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ReactGA from "react-ga4";
+
+
+
+// Initialize Google Analytics
+ReactGA.initialize("G-D9JKV7YKZX");
+
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: "/my-path", 
+  title: "Custom Title" 
+});
 
 ReactDOM.render(
   <BrowserRouter>
